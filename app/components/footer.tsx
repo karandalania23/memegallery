02b90eb25b48aa3afe3f82a0deb22570579cefbd
Karan Dalania <karandalania.kd@gmail.com>
2023-08-26 22:09:20 +0530

    :bug: Bug fix

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer () {
             <div className='text-2xl font-medium'>
             Thank You for visiting !!!
             </div>
-            <hr className='h-[2px] w-96 m-auto bg-black'/>
+            <hr className='h-[2px] w-full md:w-96 m-auto bg-black'/>
             <div className=' font-medium'>
             Made by <a href='https://github.com/karandalania23' target='_blank' className='hover:text-gray-500'>Karan Dalania</a>
             </div>
